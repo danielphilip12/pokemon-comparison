e1afaa5521540d4966b5936fd6ae4c181f271910
Daniel Gallo <daniel.gallo12@gmail.com>
2020-08-16 22:25:34 -0700

    Ability works

--- src/components/Abilities.vue
+++ src/components/Abilities.vue
@@ -8,18 +8,18 @@
                 <tr>
                     <th>Name</th>
                     <th>Hidden</th>
-                    <!-- <th>Button</th>
-                    <th>Effect</th> -->
+                    <th>Effect</th>
+                    
                 </tr>
             </thead>
             <tbody>
                 <tr v-for="(ability, index) in abilities" :key="index">
                     <td>{{ ability.ability.name }}</td>
                     <td>{{ ability.is_hidden }}</td>
+                    <Ability :ability="ability.ability.name" :pokedex="pokedex" />
                     <!-- <td><button @click="getAbilityInfoAsync(ability.ability.name)" class="btn btn-info">See effect</button></td>
                     <td v-if="effects.length > index">{{ effects[index] }}</td> -->
-                    {{ getAbilityInfo(ability.ability.name) }}
-                    <p>Effect {{ ability_effect }}</p>
+                    
                 </tr>
             </tbody>
         </table>
@@ -29,52 +29,13 @@
 </template>
 
 <script>
+import Ability from './Ability'
 export default {
     name: "Abilities",
-    props: ["abilities", "pokedex", "effects"],
-    data() {
-        return {
-            ability_effect: ''
-        }
+    components: {
+        Ability
     },
-    methods: {
-        getEffectEntry(response) {
-            let entries = response.effect_entries;
-            let answer = "";
-            for(let i = 0; i < entries.length; i++) {
-                let x = entries[i].language                
-                if (x.name === "en") {
-                    // console.log(entries[i])
-                    answer = entries[i]["short_effect"];
-                    if (!this.effects.includes(answer)){
-                        this.effects.push(answer);
-                        return answer
-                    }
-                    
-                }
-            }
-            
-        },
-        getAbilityInfoAsync(ability) {
-            return this.pokedex.getAbilityByName(ability)
-            
-        },
-
-        getAbilityInfo(ability) {
-            return this.getAbilityInfoAsync(ability).then(response => {
-                console.log(response);
-                return this.getEffectEntry(response)
-            }).then(data => {
-                console.log(data);
-                if(data !== undefined) {
-                    this.ability_effect = data;
-                }
-            })
-        }
-
-        
-        
-    }
+    props: ["abilities", "pokedex", "effects"]
 }
 </script>
 
--- src/components/Ability.vue
+++ src/components/Ability.vue
@@ -1,11 +1,50 @@
 <template>
-  <td></td>
+  <td>{{ ability_effect }}</td>
 </template>
 
 <script>
 export default {
     name: 'Ability',
-    props: ['ability']
+    props: ['ability', 'pokedex'],
+    data() {
+        return {
+            ability_effect: ''
+        }
+    },
+    methods: {
+        getEffectEntry(response) {
+            let entries = response.effect_entries;
+            let answer = "";
+            for(let i = 0; i < entries.length; i++) {
+                let x = entries[i].language                
+                if (x.name === "en") {
+                    // console.log(entries[i])
+                    answer = entries[i]["short_effect"];
+                    return answer
+                    
+                }
+            }
+            
+        },
+        getAbilityInfoAsync(ability) {
+            return this.pokedex.getAbilityByName(ability)
+            
+        },
+
+        getAbilityInfo(ability) {
+            return this.getAbilityInfoAsync(ability).then(response => {
+                return this.getEffectEntry(response)
+            }).then(data => {
+                
+                if(data !== undefined) {
+                    this.ability_effect = data;
+                }
+            })
+        }
+    },
+    mounted: function() {
+      this.getAbilityInfo(this.ability)
+    }
 }
 </script>
 
